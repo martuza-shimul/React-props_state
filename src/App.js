bb66469d86f81a65f74925961b8193302c433508
Martuza Shimul <martuza.shimul@gmail.com>
2019-10-06 02:07:33 +0600

    props initialized

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import Hello from './sayHello';
+import Tweet from './Tweet';
 
 function App() {
 
   return(
-    <div>
-      <h1>This is the App component</h1>
-      <Hello />
+    <div className="app">
+      
+      <Tweet name="Martuza Shimul" message="Learned a new thing in JavaScript"/>
+      <Tweet name="Samiul Islam" message="I have a new Project ;) "/>
+      <Tweet name="Nazmul Sumon" message="I'm very serious with my new Girlfriend"/>
+      <Tweet name="Abu Sufian" message="I want to marry a Smart & educated girl"/>
+      
     </div>
   )
 }
